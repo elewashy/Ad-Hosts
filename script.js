319c0e5800db2d5e2512edb6742e565cccd5c38c
elewashy <elewashy9@gmail.com>
2025-02-01 04:32:57 +0200

    Update

--- script.js
+++ script.js
@@ -635,7 +635,7 @@
                 newIframe.scrolling = "no";
                 
                 // تعطيل الروابط داخل الـ iframe
-                newIframe.sandbox = "allow-scripts"; 
+                newIframe.sandbox = "allow-scripts allow-same-origin allow-forms";
 
                 document.body.appendChild(newIframe);
             }
