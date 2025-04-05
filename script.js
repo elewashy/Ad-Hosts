bb0cd6d9b4df97c562aba3c92284fb38046e7a32
elewashy <elewashy9@gmail.com>
2025-04-05 22:36:38 +0200

    Update

--- script.js
+++ script.js
@@ -30,6 +30,7 @@
     var selectorsToRemove = [
         '.pm-ads-banner',        // Example: ad banner
         '.ad-container',
+        'adsLionz',
         // Add more selectors here, such as:
         // '.another-ad-class',
         // '#specific-ad-id',
