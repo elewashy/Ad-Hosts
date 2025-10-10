f57e6dc45d6ca87b1d375cc9639803fa41595d9d
Mohamed Tamer <elewashy9@gmail.com>
2025-10-10 19:02:27 +0300

    Update

--- script.js
+++ script.js
@@ -1065,20 +1065,23 @@
             link.href = link.href.replace("frdl.to", "frdl.io");
         }
     });
-    // function removeElements() {
-    //     var elementsToRemove = ["xqeqjp", "xqeqjp1","xqeqjp3"];
+    function hideElements() {
+        // قائمة بأسماء الـ ID للعناصر اللي عايز تخفيها
+        var elementsToHide = ["xqeqjp", "xqeqjp1", "xqeqjp3"];
         
-    //     for (var i = 0; i < elementsToRemove.length; i++) {
-    //         var elementId = elementsToRemove[i];
-    //         var element = document.getElementById(elementId);
-    //         if (element && element.parentNode) {
-    //             element.parentNode.removeChild(element);
-    //         } 
-    //     }
-    // }
-    
-    // // Execute the function
-    // removeElements();
+        for (var i = 0; i < elementsToHide.length; i++) {
+            var elementId = elementsToHide[i];
+            var element = document.getElementById(elementId);
+            
+            // لو العنصر موجود، غير خاصية العرض بتاعته لـ 'none' عشان يختفي
+            if (element) {
+                element.style.display = 'none';
+            } 
+        }
+    }
+
+    // تنفيذ الدالة
+    hideElements();
 })();
 (function () {
   const watchList = document.querySelector('#watch');
